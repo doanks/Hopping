c2943bcde62faa8aef4e1a145c5575e094715c14
doanks <doank9102@gmail.com>
2017-09-28 09:35:29 +0800

    fgdtfdh

---
+++ Assets/Scripts/TransformAnimation.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransformAnimation : MonoBehaviour {
+
+	public float speed;
+	public float distance;
+	
+	// Update is called once per frame
+	void Update () {
+
+		transform.Translate (-Vector2.right * speed * Time.deltaTime);
+	}
+}
